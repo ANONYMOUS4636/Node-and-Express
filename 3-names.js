f80523bd9d249eb5edf29d0edd086b001d2b632c
Dattatraya Behera <beheradattatraya123+github@gmail.com>
2025-12-28 11:03:12 +0530

    added global and modules

---
+++ 3-names.js
@@ -0,0 +1,10 @@
+//sharable
+const name1="dattatraya";
+const name2="behera";
+//non sharable
+const secret="secret";
+
+// console.log(modules);
+
+module.exports={name1,name2};
+
---
+++ 5-main.js
@@ -0,0 +1,6 @@
+const name=require("./3-names");
+console.log(name);
+
+const sayHi=require("./4-function");
+sayHi(name.name1);
+sayHi(name.name2);
