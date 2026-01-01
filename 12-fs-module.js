877266e31790553d52e12d7ab8d0f82e9d1b8c35
Dattatraya Behera <beheradattatraya123+github@gmail.com>
2026-01-02 00:08:20 +0530

    added fs modules

--- 12-fs-module.js
+++ 12-fs-module.js
@@ -7,3 +7,7 @@ console.log(first);
 console.log(second);
 writeFileSync("./content/result.txt",`this is the result of: 1) ${first} and 2) ${second}`);
 //if no file is there it will create one otherwise it will overwrite all content with new one
+
+writeFileSync("./content/result.txt",`this is the result of: 1) ${first} and 2) ${second}`,{flag:'a'});
+
+//it will append the text not ovwer write it
