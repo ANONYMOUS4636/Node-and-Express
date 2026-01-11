ad94a23bd69a627f3ec69d877834d76dc9496139
Dattatraya Behera <beheradattatraya123+github@gmail.com>
2026-01-11 11:15:20 +0530

    added express tutorial

--- 02-express/app.js
+++ 02-express/app.js
@@ -1,13 +1,42 @@
 const http=require('http')
-
+const fs=require('fs')
+const homePage=fs.readFileSync('./nav-bar/index.html')
+const homestyles=fs.readFileSync('./nav-bar/styles.css')
+const homelogo=fs.readFileSync('./nav-bar/logo.svg')
+const homelogic=fs.readFileSync('./nav-bar/browser-app.js')
 const server=http.createServer((req,res)=>{
-
+    const url=req.url;
     console.log(req.url);
     
-    
-    res.writeHead(200,{'content-type':'text/html'})
-    res.write('<h1>Hello there</h1> \n <h2>Welcome to home page</h2>')
-    res.end()
+    if(req.url === '/'){
+
+        res.writeHead(200,{'content-type':'text/html'})
+        res.write(homePage)
+        res.end()
+    }
+
+    else if(url==='/logo.svg'){
+        res.writeHead(200,{'content-type':'image/svg+xml'})
+        res.write(homelogo)
+        res.end()
+    }
+
+    else if(url==='/browser-app.js'){
+        res.writeHead(200,{'content-type':'text/javascript'})
+        res.write(homelogic)
+        res.end()
+    }
+
+    else if(url==='/styles.css'){
+        res.writeHead(200,{'content-type':'text/css'})
+        res.write(homestyles)
+        res.end()
+    }
+
+    else{
+        res.writeHead(404, {'content-type':'text/plain'})
+        res.end('404 page not found')
+    }
     console.log('user');
 })
 
